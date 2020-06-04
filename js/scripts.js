c87d43eed1199d9edda37bd9b46b5113a9d754b8
Payne Fulcher <pfulcher26@gmail.com>
2020-06-04 15:04:22 -0700

    Adds CSS styling for max image width, height, and auto-adjust for margin

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     const question1 = $("#bodytype").val();
     const question2 = $("input:radio[name=bodyhair]:checked").val();
     const question3 = $("input#snout").val(); 
-    const result = "My favorite body type is " + question1 + "." + "I prefer the body of my lover to be " + question2 + "." + "My favorite body part on a love interest is " + question3 + "."; 
+    const result = "My favorite body type is " + question1 + "." + " I prefer the body of my lover to be " + question2 + "." + " My favorite body part on a love interest is " + question3 + "."; 
     $("#output").text(result); 
   });  
 });
