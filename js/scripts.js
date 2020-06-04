eacdbc4c93d1bcb8976ef6eadc2cfdeadad9900d
Payne Fulcher <pfulcher26@gmail.com>
2020-06-04 14:58:02 -0700

    Removes form and form id tags from individual questions, wraps all quetions in one parent tag

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-  $"#formOne").submit(function(event) {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
     const question1 = $("#bodytype").val();
     const question2 = $("input:radio[name=bodyhair]:checked").val();
-    const question3 = $("input#snout").val() 
-
-    event.preventDefault();
+    const question3 = $("input#snout").val(); 
+    const result = "My favorite body type is " + question1 + "." + "I prefer the body of my lover to be " + question2 + "." + "My favorite body part on a love interest is " + question3 + "."; 
+    $("#output").text(result); 
   });  
-}
+});
